0c415a342fe1fa1832a91293c15b222aa437ff93
Clayton Bentes <claytonbentes@live.com>
2025-02-19 21:53:47 -0300

    feat: implementando funcao e rota get-subscriber-invites-count e get-subscriber-invite-ranking-position

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -6,11 +6,13 @@ import { redis } from '../redis/client'
 interface SubscribeToEventParams {
   name: string
   email: string
+  referrerId?: string | null
 }
 
 export async function subscribeToEvent({
   name,
   email,
+  referrerId,
 }: SubscribeToEventParams) {
   const subscribers = await db
     .select()
@@ -30,6 +32,10 @@ export async function subscribeToEvent({
       email,
     })
     .returning()
+
+  if (referrerId) {
+    await redis.zincrby('referral:ranking', 1, referrerId)
+  }
 
   const subscriber = result[0]
 

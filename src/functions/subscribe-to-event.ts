4db492c62ab4e6284ad6c386b2178c0fabdcd507
Clayton Bentes <claytonbentes@live.com>
2025-02-19 21:17:08 -0300

    feat: add rota e funcao get-subscriber-invite-clicks

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,5 +1,7 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/client'
 import { subscriptions } from '../drizzle/schema/subscriptions'
+import { redis } from '../redis/client'
 
 interface SubscribeToEventParams {
   name: string
@@ -10,6 +12,17 @@ export async function subscribeToEvent({
   name,
   email,
 }: SubscribeToEventParams) {
+  const subscribers = await db
+    .select()
+    .from(subscriptions)
+    .where(eq(subscriptions.email, email))
+
+  if (subscribers.length > 0) {
+    return {
+      subscriberId: subscribers[0].id,
+    }
+  }
+
   const result = await db
     .insert(subscriptions)
     .values({
